bc5e072f923a3251f688cca244f01091ea0e5c74
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-07-26 10:28:46 +0100

    center form

--- src/components/screens/Login/Login.js
+++ src/components/screens/Login/Login.js
@@ -10,64 +10,66 @@ import Logo from '../../common/Logo';
 
 export default function Login() {
   return (
-    <Container maxWidth="xs" fixed>
-      <Box boxShadow={1} padding={3} bgcolor="white">
-        <Grid
-          container
-          direction="column"
-          justify="center"
-        >
+    <Box display="flex" flex={1} justifyContent="center" alignItems="center">
+      <Container maxWidth="xs" fixed>
+        <Box boxShadow={1} padding={3} bgcolor="white">
           <Grid
-            item
             container
+            direction="column"
             justify="center"
-            alignItems="center"
           >
+            <Grid
+              item
+              container
+              justify="center"
+              alignItems="center"
+            >
+              <Grid item>
+                <Logo />
+              </Grid>
+            </Grid>
             <Grid item>
-              <Logo />
+              <TextField
+                id="email"
+                name="email"
+                label="Email Address"
+                variant="outlined"
+                type="email"
+                autoComplete="email"
+                margin="normal"
+                autoFocus
+                required
+                fullWidth
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                id="password"
+                name="password"
+                label="Password"
+                variant="outlined"
+                type="password"
+                autoComplete="current-password"
+                margin="normal"
+                required
+                fullWidth
+              />
+            </Grid>
+            <Grid item>
+              <Box marginTop={2}>
+                <Button
+                  disableElevation
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                >
+                  Sign In
+                </Button>
+              </Box>
             </Grid>
           </Grid>
-          <Grid item>
-            <TextField
-              id="email"
-              name="email"
-              label="Email Address"
-              variant="outlined"
-              type="email"
-              autoComplete="email"
-              margin="normal"
-              autoFocus
-              required
-              fullWidth
-            />
-          </Grid>
-          <Grid item>
-            <TextField
-              id="password"
-              name="password"
-              label="Password"
-              variant="outlined"
-              type="password"
-              autoComplete="current-password"
-              margin="normal"
-              required
-              fullWidth
-            />
-          </Grid>
-          <Grid item>
-            <Box marginTop={2}>
-              <Button
-                disableElevation
-                variant="contained"
-                color="primary"
-                fullWidth
-              >
-                Sign In
-              </Button>
-            </Box>
-          </Grid>
-        </Grid>
-      </Box>
-    </Container>
+        </Box>
+      </Container>
+    </Box>
   );
 }
