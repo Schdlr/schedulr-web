5c411bfcf343796b313f506bd990bd9821a924f5
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-07-26 11:16:36 +0100

    custom theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {ThemeProvider} from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import {Login} from './components/screens';
+import theme from './config/theme';
 
 function App() {
   return (
-    <Login />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Login />
+    </ThemeProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'fontsource-roboto';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
