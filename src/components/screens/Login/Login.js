3965642d1f96caaf82cc37cef58faed1c6cc93da
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-07-26 10:10:30 +0100

    refactor

--- src/components/screens/Login/Login.js
+++ src/components/screens/Login/Login.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
   Container,
-  Paper,
   Grid,
   Button,
   TextField,
@@ -12,65 +11,62 @@ import Logo from '../../common/Logo';
 export default function Login() {
   return (
     <Container maxWidth="xs" fixed>
-      <Box marginTop={8}>
-        <Paper>
-          <Box padding={3}>
-            <Grid
-              container
-              direction="column"
-              justify="center"
-            >
-              <Grid
-                item
-                container
-                justify="center"
-                alignItems="center"
-              >
-                <Grid item>
-                  <Logo />
-                </Grid>
-              </Grid>
-              <Grid item>
-                <TextField
-                  id="email"
-                  name="email"
-                  label="Email Address"
-                  variant="outlined"
-                  type="email"
-                  autoComplete="email"
-                  margin="normal"
-                  autoFocus
-                  required
-                  fullWidth
-                />
-              </Grid>
-              <Grid item>
-                <TextField
-                  id="password"
-                  name="password"
-                  label="Password"
-                  variant="outlined"
-                  type="password"
-                  autoComplete="current-password"
-                  margin="normal"
-                  required
-                  fullWidth
-                />
-              </Grid>
-              <Grid item>
-                <Box marginTop={2}>
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    fullWidth
-                  >
-                    Sign In
-                  </Button>
-                </Box>
-              </Grid>
+      <Box boxShadow={1} padding={3} marginTop={8}>
+        <Grid
+          container
+          direction="column"
+          justify="center"
+        >
+          <Grid
+            item
+            container
+            justify="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <Logo />
             </Grid>
-          </Box>
-        </Paper>
+          </Grid>
+          <Grid item>
+            <TextField
+              id="email"
+              name="email"
+              label="Email Address"
+              variant="outlined"
+              type="email"
+              autoComplete="email"
+              margin="normal"
+              autoFocus
+              required
+              fullWidth
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="password"
+              name="password"
+              label="Password"
+              variant="outlined"
+              type="password"
+              autoComplete="current-password"
+              margin="normal"
+              required
+              fullWidth
+            />
+          </Grid>
+          <Grid item>
+            <Box marginTop={2}>
+              <Button
+                disableElevation
+                variant="contained"
+                color="primary"
+                fullWidth
+              >
+                Sign In
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
       </Box>
     </Container>
   );
